95a4da9e4fe938e05dfe92c3dd435f80e7e56c22
crayon.meng <crayon.meng@hytechc.com>
2025-03-28 16:52:36 +0800

    增加api请求 - meng-hub-crayon

---
+++ hub-common/hub-common-core/src/main/java/com/crayon/common/core/util/http/OkHttpUtil.java
@@ -0,0 +1,108 @@
+package com.crayon.common.core.util.http;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author Mengdl
+ * @date 2025/03/27
+ */
+@Slf4j
+public class OkHttpUtil {
+
+    private static final OkHttpClient CLIENT = new OkHttpClient();
+    private static final MediaType JSON_MEDIA_TYPE = MediaType.get("application/json; charset=utf-8");
+
+    public static String get(String url, String jsonParam, String jsonHeader) {
+        Request.Builder requestBuilder = new Request.Builder()
+                .url(url + convertGetParams(jsonParam))
+                .get();
+        Headers headers = convertHeader(jsonHeader);
+        if (headers != null) {
+            requestBuilder.headers(headers);
+        }
+        Request request = requestBuilder.build();
+        try (Response response = CLIENT.newCall(request).execute()) {
+            return response.body() != null ? response.body().string() : null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String post(String url, String jsonBody, String jsonHeader) {
+        RequestBody body = RequestBody.create(jsonBody, JSON_MEDIA_TYPE);
+        Request.Builder requestBuilder = new Request.Builder()
+                .url(url)
+                .post(body);
+        Headers headers = convertHeader(jsonHeader);
+        if (headers != null) {
+            requestBuilder.headers(headers);
+        }
+        Request request = requestBuilder.build();
+        try (Response response = CLIENT.newCall(request).execute()) {
+            return response.body() != null ? response.body().string() : null;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 处理get请求参数
+     *
+     * @param jsonParam 请求jason 参数
+     * @return 结果
+     */
+    public static String convertGetParams(String jsonParam) {
+        if (jsonParam == null || jsonParam.isEmpty()) {
+            return null;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        jsonConvertMap(jsonParam).forEach((k, v) -> stringBuilder.append('&').append(k).append('=').append(v));
+        if (!stringBuilder.isEmpty()) {
+            stringBuilder.setCharAt(0, '?');
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * 处理header
+     *
+     * @param jsonHeader jsonHeader
+     * @return headers
+     */
+    public static Headers convertHeader(String jsonHeader) {
+        if (jsonHeader == null || jsonHeader.isEmpty()) {
+            return null;
+        }
+        Headers.Builder headerBuilder = new Headers.Builder()
+                .set("Content-Type", "application/json");
+        jsonConvertMap(jsonHeader).forEach((k, v) -> headerBuilder.set(k, v.toString()));
+        return headerBuilder.build();
+    }
+
+    /**
+     * json转map
+     *
+     * @param jsonStr json字符串
+     * @return map
+     */
+    public static Map<String, Object> jsonConvertMap(String jsonStr) {
+        return JSON.parseObject(jsonStr, new TypeReference<>() {
+        });
+    }
+
+    public static void main(String[] args) {
+        String url = "https://new-mts-api.app-alpha.com/account/account/detail";
+        String jsonParam = "{\"accountNo\":\"16456032\"}";
+        String jsonHeader = "{\"Authorization\":\"Bearer 1f8b0004-f2d7-4e67-86ab-b37dcd78ef9b\", \"tenant-id\":\"1\"}";
+        String result = get(url, jsonParam, jsonHeader);
+        System.out.println(result);
+    }
+
+}
--- hub-common/hub-common-core/src/main/java/com/crayon/common/core/util/http/RestTemplateUtil.java
+++ hub-common/hub-common-core/src/main/java/com/crayon/common/core/util/http/RestTemplateUtil.java
@@ -1,9 +1,13 @@
 package com.crayon.common.core.util.http;
 
+import com.alibaba.fastjson2.JSON;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
+
+import static com.crayon.common.core.util.http.OkHttpUtil.convertGetParams;
+import static com.crayon.common.core.util.http.OkHttpUtil.jsonConvertMap;
 
 /**
  * @author Mengdl
@@ -16,9 +20,9 @@ public class RestTemplateUtil {
     /**
      * 发送GET请求
      *
-     * @param url 请求的URL
+     * @param url          请求的URL
      * @param responseType 返回类型
-     * @param <T> 泛型类型
+     * @param <T>          泛型类型
      * @return 响应结果
      */
     public static <T> T get(String url, Class<T> responseType) {
@@ -26,33 +30,66 @@ public class RestTemplateUtil {
     }
 
     /**
-     * 发送GET请求，携带参数
+     * 发送GET请求
      *
-     * @param url 请求的URL
-     * @param uriVariables 请求参数
+     * @param url          请求的URL
      * @param responseType 返回类型
-     * @param <T> 泛型类型
+     * @param headers      请求头信息
+     * @param <T>          泛型类型
      * @return 响应结果
      */
-    public static <T> T get(String url, Map<String, ?> uriVariables, Class<T> responseType) {
-        return REST_TEMPLATE.getForObject(url, responseType, uriVariables);
+    public static <T> T get(String url, Class<T> responseType, Map<String, String> headers) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAll(headers);
+        return REST_TEMPLATE.exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders), responseType).getBody();
     }
 
     /**
-     * 发送POST请求
+     * 发送GET请求
      *
-     * @param url 请求的URL
-     * @param requestBody 请求体
+     * @param url          请求的URL
      * @param responseType 返回类型
-     * @param <T> 泛型类型
+     * @param jsonHeader   请求头信息
+     * @param <T>          泛型类型
      * @return 响应结果
      */
+    public static <T> T get(String url, Class<T> responseType, String jsonHeader) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        jsonConvertMap(jsonHeader).forEach((k, v) -> httpHeaders.add(k, v.toString()));
+        return REST_TEMPLATE.exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders), responseType).getBody();
+    }
+
+    public static <T> T get(String url, Class<T> responseType, String jsonHeader, String uriVariables) {
+        String queryParams = convertGetParams(uriVariables);
+        url = url + queryParams;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        jsonConvertMap(jsonHeader).forEach((k, v) -> httpHeaders.add(k, v.toString()));
+        return REST_TEMPLATE.exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders), responseType).getBody();
+    }
+
+    public static <T> T get(String url, Class<T> responseType, String jsonHeader, Map<String, ?> uriVariables) {
+        String queryParams = convertGetParams(JSON.toJSONString(uriVariables));
+        url = url + queryParams;
+        HttpHeaders httpHeaders = new HttpHeaders();
+        jsonConvertMap(jsonHeader).forEach((k, v) -> httpHeaders.add(k, v.toString()));
+        return REST_TEMPLATE.exchange(url, HttpMethod.GET, new HttpEntity<>(httpHeaders), responseType).getBody();
+
+    }
+
     public static <T> T post(String url, Object requestBody, Class<T> responseType) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<Object> entity = new HttpEntity<>(requestBody, headers);
         ResponseEntity<T> response = REST_TEMPLATE.exchange(url, HttpMethod.POST, entity, responseType);
         return response.getBody();
+    }
+
+    public static void main(String[] args) {
+        String url = "https://new-mts-api.app-alpha.com/account/account/detail";
+        String jsonParam = "{\"accountNo\":\"16456032\"}";
+        String jsonHeader = "{\"Authorization\":\"Bearer 1f8b0004-f2d7-4e67-86ab-b37dcd78ef9b\", \"tenant-id\":\"1\"}";
+        String result = get(url, String.class, jsonHeader, jsonParam);
+        System.out.println(result);
     }
 
 }
