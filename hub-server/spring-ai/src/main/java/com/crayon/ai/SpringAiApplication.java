dd94e2971dd47e4ac90e74cc45c58ee1ea6aaffa
mengdl <mengdl@cheche365.com>
2025-07-26 16:56:01 +0800

    修改版本号 -interview-service

---
+++ hub-server/spring-ai/src/main/java/com/crayon/ai/SpringAiApplication.java
@@ -0,0 +1,19 @@
+package com.crayon.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @Description
+ * @Author Administrator
+ * @Date 2024/4/16 13:52
+ **/
+@SpringBootApplication
+public class SpringAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiApplication.class, args);
+    }
+
+}
---
+++ hub-server/spring-ai/src/main/java/com/crayon/ai/controller/AiController.java
@@ -0,0 +1,16 @@
+package com.crayon.ai.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ *
+ * @Description
+ * @Author Administrator
+ * @Date 2025/7/26 16:40
+ **/
+@RestController
+public class AiController {
+
+}
